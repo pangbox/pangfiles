39d6dad840eca0f5ee1f3d3a939683be99c6ecbd
John Chadwick <john@jchw.io>
2020-06-28 23:22:00 -0700

    Add updatelist command with updatelist server.

---
+++ updatelist/xml.go
@@ -0,0 +1,74 @@
+package updatelist
+
+import (
+	"hash/crc32"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/pangbox/pangfiles/encoding/litexml"
+	"github.com/pangbox/pangfiles/hash/pycrc32"
+)
+
+const crcBufLen = 2 << 11
+
+// FileInfo represents the per-file information in an UpdateList.
+type FileInfo struct {
+	Filename   string `attr:"fname"`
+	Directory  string `attr:"fdir"`
+	Size       int64  `attr:"fsize"`
+	Crc        int32  `attr:"fcrc"`
+	Date       string `attr:"fdate"`
+	Time       string `attr:"ftime"`
+	PackedName string `attr:"pname"`
+	PackedSize int64  `attr:"psize"`
+}
+
+// UpdateFiles contains the files in the updatelist.
+type UpdateFiles struct {
+	Count int        `attr:"count"`
+	Files []FileInfo `tag:"fileinfo"`
+}
+
+// Document is the document containing the update list.
+type Document struct {
+	Info          litexml.DocumentInfo
+	PatchVer      string      `tag:"patchVer" attr:"value"`
+	PatchNum      int         `tag:"patchNum" attr:"value"`
+	UpdateListVer string      `tag:"updatelistVer" attr:"value"`
+	UpdateFiles   UpdateFiles `tag:"updatefiles"`
+}
+
+// MakeFileInfo generates a FileInfo structure from an OS file.
+func MakeFileInfo(basedir, dir string, finfo os.FileInfo, psize int64) (FileInfo, error) {
+	name := finfo.Name()
+	f, err := os.Open(filepath.Join(basedir, dir, name))
+	if err != nil {
+		return FileInfo{}, err
+	}
+
+	crc := uint32(0)
+	flen := 0
+	for {
+		buf := [crcBufLen]byte{}
+		c, err := f.Read(buf[:])
+		if c == 0 && err == io.EOF {
+			break
+		} else if err != nil {
+			return FileInfo{}, err
+		}
+		crc = crc32.Update(crc, pycrc32.FileTable, buf[:c])
+		flen += c
+	}
+
+	return FileInfo{
+		Filename:   name,
+		Directory:  dir,
+		Size:       finfo.Size(),
+		Crc:        int32(crc),
+		Date:       finfo.ModTime().Format("2006-01-02"),
+		Time:       finfo.ModTime().Format("15:04:05"),
+		PackedName: name + ".zip",
+		PackedSize: psize,
+	}, nil
+}
--- util/pad.go
+++ util/pad.go
@@ -5,12 +5,12 @@ import "io"
 // NullInputPadder pads all short reads to return zeros. This is useful for
 // doing streaming encryption using XTEA, for example.
 type NullInputPadder struct {
-	r io.Reader
+	Reader io.Reader
 }
 
 // Read implements io.Reader.
 func (i NullInputPadder) Read(p []byte) (n int, err error) {
-	n, err = i.r.Read(p)
+	n, err = i.Reader.Read(p)
 	if n > 0 && n < len(p) {
 		for ; n < len(p); n++ {
 			p[n] = 0
